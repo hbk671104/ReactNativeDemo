c69ecec4318bcfcee160cd2cbe74214587f3d2fb
hbk671104 <hbk671104@gmail.com>
2016-08-15 18:24:57 +0800

    update ios

--- index.ios.js
+++ index.ios.js
@@ -5,84 +5,32 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  ListView,
-  View,
-  Text,
-  StyleSheet,
-  Image} from 'react-native';
+import { AppRegistry, Navigator } from 'react-native';
+import HeadlineListScene from './HeadlineListScene';
+import HeadlineDetailScene from './HeadlineDetailScene';
 
 class ReactNativeDemo extends Component {
 
- constructor(props) {
-   super(props);
-   const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
-   this.state = {
-     dataSource: ds.cloneWithRows([''])
-   };
- }
+  render() {
+    return (
+      <Navigator
+        initialRoute={{name: 'HeadlineListScene', passInObject: 'object'}}
+        renderScene={this.renderScene.bind(this)}
+      />
+    );
+  }
 
- componentWillMount() {
-   this.getHeaderline();
- }
-
- render() {
-   return (
-     <View style={styles.container}>
-       <ListView
-         dataSource={this.state.dataSource}
-         renderRow={this.customizedRenderRow}
-       />
-     </View>
-   );
- }
-
- customizedRenderRow(rowData) {
-   return (
-     <View style={styles.container}>
-       <Image style={styles.image} source={{uri: rowData.preview}}>
-         <View style={styles.alphaContainer}>
-           <Text style={styles.text}>{rowData.title}</Text>
-         </View>
-       </Image>
-     </View>
-   );
- }
-
- getHeaderline() {
-   fetch('http://web.meishuquan.net/rest/headline/get-headline-list?category=161&limit=20&headline_id=0')
-     .then((response) => response.json())
-     .then((responseJson) => {
-       this.setState({
-         dataSource: this.state.dataSource.cloneWithRows(responseJson.data)
-       })
-     })
-     .catch((error) => {
-       console.error(error);
-     })
- }
+  renderScene(route, navigator) {
+    switch (route.name) {
+      case 'HeadlineListScene':
+        return (<HeadlineListScene navigator={navigator}
+          passInObject={route.passInObject}/>);
+      case 'HeadlineDetailScene':
+        return (<HeadlineDetailScene navigator={navigator}
+          passInObject={route.passInObject}/>);
+    }
+  }
 
 }
-
-const styles = StyleSheet.create({
- container: {
-   flex: 1
- },
- alphaContainer: {
-   backgroundColor: 'rgba(0,0,0,0.5)'
- },
- text: {
-   textAlign: 'auto',
-   fontSize: 20,
-   color: 'white',
-   marginHorizontal: 10,
-   marginVertical: 5
- },
- image: {
-   justifyContent: 'flex-end',
-   height: 250
- }
-});
 
 AppRegistry.registerComponent('ReactNativeDemo', () => ReactNativeDemo);
